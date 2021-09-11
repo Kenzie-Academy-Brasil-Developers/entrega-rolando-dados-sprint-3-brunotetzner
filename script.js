4c09763d2c1720a36327bd79a60eedabc4a66d68
Bruno Tetzner <brunoltetzner@gmail.com>
2021-09-11 11:14:52 -0300

    Remoção de variáveis desnecessárias, aprimoramento da legibilidade(comentários e identação)

--- script.js
+++ script.js
@@ -1,47 +1,33 @@
-//Escrever o Array na tela usando o DOM
-// Criar uma barra que represente o valor do array 
+//script para adicionar o paragrafos com os valores do array e a barra 
 
-
-
-let repete = 1000;
 let count = [0,0,0,0,0,0,0,0,0,0,0,0,0]
 let containerresults = document.getElementById('container')
 
-
+//Função que gera um numero aleatório
 function randomnumber(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-  }
-  
-//criar o loop pra os dados 
-//Escrever cada valor na tela
-//EM frente a cada 
-
-for(let i = 0; i <repete;i++){
-let number = randomnumber(1,6) + randomnumber(1,6)
-count[number] = count[number] + 1;
- 
+ min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
-console.log(count)
-//Pegar o valor de cada item do array e criar um paragrafo apra ele
-//Criar a barra 
-//criar uma div
-//colocar ambos dentro da div
-//colocar a div no container
 
+  //Loop para gerar  o array
+for(let i = 0; i <1000;i++){
+  let number = randomnumber(1,6) + randomnumber(1,6)
+  count[number] = count[number] + 1;
+ }
+
+//Loop para escrver os resultados na tela
 for(let i = 0; i<count.length;i++){
 
-let writeitem = document.createElement('p')
-let sectionitems = document.createElement('section') // div par armazenar o valor do numero e a div da barra
-let minhabarra  =document.createElement('div')
-let larguraDaBarra = count[i]
-minhabarra.style.width = `${larguraDaBarra}px`;
+  let writeitem = document.createElement('p')
+  let sectionitems = document.createElement('section') // div par armazenar o valor do numero e a div da barra
+  let minhabarra  =document.createElement('div')
+  minhabarra.style.width = `${count[i]}px`;
 
-sectionitems.appendChild(writeitem)
-containerresults.appendChild(sectionitems)
-writeitem.innerText = count[i]
-sectionitems.appendChild(minhabarra)
+  sectionitems.appendChild(writeitem)
+  containerresults.appendChild(sectionitems)
+  writeitem.innerText = count[i]
+  sectionitems.appendChild(minhabarra)
 }
 
 
